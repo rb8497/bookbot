e8a653e4218ca8f7623903880157051ac5da8f1c
rb97 <rbulter8497@yahoo.com>
2025-06-04 19:55:31 -0500

    make read function in main.py

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("greetings boots")
+def get_book_text(book):
+    with open(book) as book_to_read:
+        book_text = book_to_read.read()
+    return book_text
+
+text = ""
+def main():
+    text = get_book_text('books/frankenstein.txt')
+    print(text)
+
+main()
